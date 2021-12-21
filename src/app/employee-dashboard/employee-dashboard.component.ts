a2f069ea10cd8d18e8fcecfef01f3ea5eceb4639
Kong310544 <kong310544@gmail.com>
2021-12-21 17:32:05 +0700

    Ts-Employee-Week8

---
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit } from '@angular/core';
+import{FormGroup, FormControl} from '@angular/forms'
+import { EmployeeModels } from './employee-dashboard.models';
+import {ApiService} from '../shared/api.service'
+import Swal from 'sweetalert2';
+@Component({
+  selector: 'app-employee-dashboard',
+  templateUrl: './employee-dashboard.component.html',
+  styleUrls: ['./employee-dashboard.component.css']
+
+})
+export class EmployeeDashboardComponent implements OnInit {
+
+  formEmployee!: FormGroup;
+  employeeModel:EmployeeModels  = new EmployeeModels();
+
+  employeeData!:any;
+  showAdd!:boolean;
+  showUpdate!:boolean;
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.formEmployee = new FormGroup({ 
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    email: new FormControl(),
+    telephone: new FormControl(),
+    salary: new FormControl()
+    
+  }),
+  this.getEmployee()
+   
+  }
+
+  postEmployee(){
+    this.employeeModel.firstName = this.formEmployee.value.firstName
+    this.employeeModel.lastName = this.formEmployee.value.lastName
+    this.employeeModel.email = this.formEmployee.value.email
+    this.employeeModel.telephone = this.formEmployee.value.telephone
+    this.employeeModel.salary = this.formEmployee.value.salary
+
+    this.api.postEmployee(this.employeeModel)
+    .subscribe(res=>{
+      Swal.fire("Complete","Add Employee Complsete", "success");
+      this.getEmployee()
+      let close = document.getElementById("close")
+      close!.click()
+    },
+    err=>{
+      Swal.fire("Error","Add Employee Error","error");
+    })
+
+  }
+
+  getEmployee(){
+    this.api.getEmployee()
+    .subscribe(res=>{
+     this.employeeData = res;
+    
+    },)
+  }
+
+  deleteEmployee(id:number){
+    this.api.deleteEmployee(id)
+    .subscribe(res=>{
+      Swal.fire("Complete","Add Employee Complsete", "success");
+      this.getEmployee()
+    },
+    err=>{
+      Swal.fire("Error","Add Employee Error","error");
+    })
+    }
+
+    clickAdd(){
+      this.formEmployee.reset()
+      this.showAdd = true
+      this.showUpdate = false
+      this.employeeData.id = 0
+    }
+
+    clickEdit(data:any){
+      this.showAdd = false
+      this.showUpdate = true
+      this.employeeData.id = data.id
+      this.formEmployee.controls['firstName'].setValue(data.firstName)
+      this.formEmployee.controls['lastName'].setValue(data.lastName)
+      this.formEmployee.controls['email'].setValue(data.email)
+      this.formEmployee.controls['telephone'].setValue(data.telephone)
+      this.formEmployee.controls['salary'].setValue(data.salary)
+    }
+
+
+    updateEmployee(){
+    this.employeeModel.firstName = this.formEmployee.value.firstName
+    this.employeeModel.lastName = this.formEmployee.value.lastName
+    this.employeeModel.email = this.formEmployee.value.email
+    this.employeeModel.telephone = this.formEmployee.value.telephone
+    this.employeeModel.salary = this.formEmployee.value.salary
+    this.api.updateEmployee(this.employeeData.id,this.employeeModel)
+    .subscribe(res=>{
+      Swal.fire("Complete","Update Employee Complsete", "success");
+      this.getEmployee()
+      let close = document.getElementById("close")
+      close!.click()
+    },
+    err=>{
+      Swal.fire("Error","Update Employee Error","error");
+    })
+    }
+}
---
+++ src/app/employee-dashboard/employee-dashboard.models.ts
@@ -0,0 +1,11 @@
+export class EmployeeModels{
+    
+    id:number = 0;
+    firstName: string ='';
+    lastName: string ='';
+    email: string ='';
+    telephone: string ='';
+    salary: number = 0
+
+
+}
